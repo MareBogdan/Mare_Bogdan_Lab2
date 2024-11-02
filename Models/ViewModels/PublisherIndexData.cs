b452d465f6d9b5e2d16b9a889f0b7df0d8030c86
MareBogdan <marebogdan03@gmail.com>
2024-11-02 13:11:28 +0200

    Laborator4_part1

---
+++ Models/ViewModels/PublisherIndexData.cs
@@ -0,0 +1,10 @@
+﻿using Mare_Bogdan_Lab2.Models;
+
+namespace Mare_Bogdan_Lab2.ViewModels
+{
+    public class PublisherIndexData
+    {
+        public IEnumerable<Publisher> Publishers { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+    }
+}
