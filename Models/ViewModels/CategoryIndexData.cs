7939558386c95ff95680b738a273713db46bcb4b
MareBogdan <marebogdan03@gmail.com>
2024-11-03 20:14:02 +0200

    sarcina de lab rezolvata

---
+++ Models/ViewModels/CategoryIndexData.cs
@@ -0,0 +1,9 @@
+﻿using Mare_Bogdan_Lab2.Models;
+namespace Mare_Bogdan_Lab2.Models.ViewModels
+{
+    public class CategoryIndexData
+    {
+        public IEnumerable<Category> Categories { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+    }
+}
