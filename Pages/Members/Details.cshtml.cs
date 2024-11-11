4365c453150138118ed3843e7f61214ead9a996a
MareBogdan <marebogdan03@gmail.com>
2024-11-11 14:41:20 +0200

    part1

---
+++ Pages/Members/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Mare_Bogdan_Lab2.Data;
+using Mare_Bogdan_Lab2.Models;
+
+namespace Mare_Bogdan_Lab2.Pages.Members
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Mare_Bogdan_Lab2.Data.Mare_Bogdan_Lab2Context _context;
+
+        public DetailsModel(Mare_Bogdan_Lab2.Data.Mare_Bogdan_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public Member Member { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var member = await _context.Member.FirstOrDefaultAsync(m => m.ID == id);
+            if (member == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Member = member;
+            }
+            return Page();
+        }
+    }
+}
