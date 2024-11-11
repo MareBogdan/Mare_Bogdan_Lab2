1ad96856615a2f23713a816bc28ad1f82fdcbbf2
MareBogdan <marebogdan03@gmail.com>
2024-11-11 09:23:26 +0200

    lab5_ex12

---
+++ Pages/Borrowings/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Mare_Bogdan_Lab2.Data;
+using Mare_Bogdan_Lab2.Models;
+
+namespace Mare_Bogdan_Lab2.Pages.Borrowings
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Mare_Bogdan_Lab2.Data.Mare_Bogdan_Lab2Context _context;
+
+        public DetailsModel(Mare_Bogdan_Lab2.Data.Mare_Bogdan_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public Borrowing Borrowing { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var borrowing = await _context.Borrowing.FirstOrDefaultAsync(m => m.ID == id);
+            if (borrowing == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Borrowing = borrowing;
+            }
+            return Page();
+        }
+    }
+}
